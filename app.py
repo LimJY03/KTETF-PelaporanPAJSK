1f07e87d8d4f1b092ea464560ccc7195ea62a718
Lim JY <limjunyi03@gmail.com>
2024-09-27 01:26:14 +0000

    Complete v1

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from login import user_login
 
 # Page config
 st.set_page_config(
@@ -6,6 +7,8 @@ st.set_page_config(
     # page_icon='maths',
     layout='wide',
 )
+
+if not user_login(): st.stop()
 
 # Page title
 st.title('Pengiraan PAJSK KTETF')
@@ -21,147 +24,217 @@ user_data = {
     'Nama Projek': ['', '', ''],
     'Jawatan Projek': ['', '', ''],
     'Peringkat Projek': ['', '', ''],
-    'Ekstra Kokurikulum': [0, 0, 0]
+    'Ekstra Kurikulum Perkhidmatan': '',
+    'Ekstra Kurikulum Anugerah Khas': '',
 }
 
 # Constants
 JAWATAN_KELAB = {
+    'Tiada': 0,
     'Pengerusi': 10,
     'Timbalan Pengerusi / Naib Pengerusi': 8,
     'Setiausaha / Bendahari': 7,
     'Penolong Setiausaha / Penolong Bendahari': 6,
     'Ahli Jawatankuasa': 5,
-    'Ahli Aktif': 4
+    'Ahli Aktif': 4,
 }
 
 PERINGKAT_KELAB = {
+    'Tiada': 0,
     'Kebangsaan': 10,
     'Negeri': 8,
     'Daerah': 6,
-    'Sekolah': 4
+    'Sekolah': 4,
 }
 
 PENCAPAIAN = {
     'Johan': 0,
     'Naib Johan': -1,
-    'Ketiga': -2
+    'Ketiga': -2,
+}
+
+PENCAPAIAN_KELAB = {'Tiada': 0} | {
+    f'{kelab} - {pencapaian}': start_value + adjust_value 
+    for kelab, start_value in list(PERINGKAT_KELAB.items())[1:] 
+    for pencapaian, adjust_value in PENCAPAIAN.items()
 }
 
 JAWATAN_PROJEK = {
+    'Tiada': 0,
     'Pengerusi Projek': 10,
     'Timbalan Pengerusi / Naib Pengerusi / Setiausaha Projek': 9,
     'Bendahari Projek': 8,
     'Penolong Setiausaha / Penolong Bendahari Projek': 7,
     'Ahli Jawatankuasa Projek': 6,
-    'Ahli': 5
+    'Ahli': 5,
 }
 
 PERINGKAT_PROJEK = {
+    'Tiada': 0,
     'Negeri': 10,
     'Daerah': 8,
-    'PTE/KTE': 6
+    'PTE/KTE': 6,
 }
 
-EXTRA_KOKO = {
-    '': 0,
+EXTRA_KOKO_PERKHIDMATAN = {
+    'Tiada': 0,
+    'Ketua Murid / Pengerusi': 10,
+    'Penolong Ketua Murid / Timbalan Pengerusi': 8,
+}
+
+EXTRA_KOKO_ANUGERAH = {
+    'Tiada': 0,
+    'ARP Emas': 10,
+    'ARP Perak': 7,
+    'ARP Gangsa': 5,
 }
 
 # Logic
-tab1, tab2, tab3 = st.tabs(['SUKAN/PERMAINAN', 'KELAB/PERSATUAN', 'BADAN BERUNIFORM'])
+tab1, tab2, tab3, tab4 = st.tabs(['SUKAN/PERMAINAN', 'KELAB/PERSATUAN', 'BADAN BERUNIFORM', 'EKSTRA KURIKULUM'])
 
 with tab1:
     col1, col2 = st.columns(2)
     with col1: 
         user_data['Kehadiran Kelab'][0] = st.number_input('Kehadiran Kelab', max_value=20, step=1, key='Kehadiran Kelab 0')
         user_data['Jawatan Kelab'][0] = st.selectbox('Jawatan Kelab', options=JAWATAN_KELAB.keys(), key='Jawatan Kelab 0')
-        user_data['Peringkat Kelab'][0] = st.selectbox('Peringkat Kelab', options=PERINGKAT_KELAB.keys(), key='Peringkat Kelab 0')
-        user_data['Peringkat Pencapaian Kelab'][0] = st.selectbox('Peringkat Pencapaian Kelab', options=PERINGKAT_KELAB.keys(), key='Peringkat Pencapaian Kelab 0')
-        user_data['Pencapaian Kelab'][0] = st.selectbox('Pencapaian Kelab', options=PENCAPAIAN.keys(), key='Pencapaian Kelab 0')
+        user_data['Peringkat Kelab'][0] = st.selectbox('Peringkat', options=PERINGKAT_KELAB.keys(), key='Peringkat Kelab 0')
+        user_data['Peringkat Pencapaian Kelab'][0] = st.selectbox('Peringkat Pencapaian', options=PENCAPAIAN_KELAB.keys(), key='Peringkat Pencapaian Kelab 0')
     with col2:
         user_data['Nama Projek'][0] = st.text_input('Nama Projek', key='Nama Projek 0')
         user_data['Jawatan Projek'][0] = st.selectbox('Jawatan Projek', options=JAWATAN_PROJEK.keys(), key='Jawatan Projek 0')
         user_data['Peringkat Projek'][0] = st.selectbox('Peringkat Projek', options=PERINGKAT_PROJEK.keys(), key='Peringkat Projek 0')
-        user_data['Ekstra Kokurikulum'][0] = st.selectbox('Ekstra Kokurikulum', options=EXTRA_KOKO.keys(), key='Ekstra Kokurikulum 0')
 
 with tab2:
     col1, col2 = st.columns(2)
     with col1: 
         user_data['Kehadiran Kelab'][1] = st.number_input('Kehadiran Kelab', max_value=20, step=1, key='Kehadiran Kelab 1')
         user_data['Jawatan Kelab'][1] = st.selectbox('Jawatan Kelab', options=JAWATAN_KELAB.keys(), key='Jawatan Kelab 1')
-        user_data['Peringkat Kelab'][1] = st.selectbox('Peringkat Kelab', options=PERINGKAT_KELAB.keys(), key='Peringkat Kelab 1')
-        user_data['Peringkat Pencapaian Kelab'][1] = st.selectbox('Peringkat Pencapaian Kelab', options=PERINGKAT_KELAB.keys(), key='Peringkat Pencapaian Kelab 1')
-        user_data['Pencapaian Kelab'][1] = st.selectbox('Pencapaian Kelab', options=PENCAPAIAN.keys(), key='Pencapaian Kelab 1')
+        user_data['Peringkat Kelab'][1] = st.selectbox('Peringkat', options=PERINGKAT_KELAB.keys(), key='Peringkat Kelab 1')
+        user_data['Peringkat Pencapaian Kelab'][1] = st.selectbox('Peringkat Pencapaian', options=PENCAPAIAN_KELAB.keys(), key='Peringkat Pencapaian Kelab 1')
     with col2:
         user_data['Nama Projek'][1] = st.text_input('Nama Projek', key='Nama Projek 1')
         user_data['Jawatan Projek'][1] = st.selectbox('Jawatan Projek', options=JAWATAN_PROJEK.keys(), key='Jawatan Projek 1')
         user_data['Peringkat Projek'][1] = st.selectbox('Peringkat Projek', options=PERINGKAT_PROJEK.keys(), key='Peringkat Projek 1')
-        user_data['Ekstra Kokurikulum'][1] = st.selectbox('Ekstra Kokurikulum', options=EXTRA_KOKO.keys(), key='Ekstra Kokurikulum 1')
 
 with tab3:
     col1, col2 = st.columns(2)
     with col1: 
         user_data['Kehadiran Kelab'][2] = st.number_input('Kehadiran Kelab', max_value=20, step=1, key='Kehadiran Kelab 2')
         user_data['Jawatan Kelab'][2] = st.selectbox('Jawatan Kelab', options=JAWATAN_KELAB.keys(), key='Jawatan Kelab 2')
-        user_data['Peringkat Kelab'][2] = st.selectbox('Peringkat Kelab', options=PERINGKAT_KELAB.keys(), key='Peringkat Kelab 2')
-        user_data['Peringkat Pencapaian Kelab'][2] = st.selectbox('Peringkat Pencapaian Kelab', options=PERINGKAT_KELAB.keys(), key='Peringkat Pencapaian Kelab 2')
-        user_data['Pencapaian Kelab'][2] = st.selectbox('Pencapaian Kelab', options=PENCAPAIAN.keys(), key='Pencapaian Kelab 2')
+        user_data['Peringkat Kelab'][2] = st.selectbox('Peringkat', options=PERINGKAT_KELAB.keys(), key='Peringkat Kelab 2')
+        user_data['Peringkat Pencapaian Kelab'][2] = st.selectbox('Peringkat Pencapaian', options=PENCAPAIAN_KELAB.keys(), key='Peringkat Pencapaian Kelab 2')
     with col2:
         user_data['Nama Projek'][2] = st.text_input('Nama Projek', key='Nama Projek 2')
         user_data['Jawatan Projek'][2] = st.selectbox('Jawatan Projek', options=JAWATAN_PROJEK.keys(), key='Jawatan Projek 2')
         user_data['Peringkat Projek'][2] = st.selectbox('Peringkat Projek', options=PERINGKAT_PROJEK.keys(), key='Peringkat Projek 2')
-        user_data['Ekstra Kokurikulum'][2] = st.selectbox('Ekstra Kokurikulum', options=EXTRA_KOKO.keys(), key='Ekstra Kokurikulum 2')
 
-# Table Display
+with tab4:
+    user_data['Ekstra Kurikulum Perkhidmatan'] = st.selectbox('Ekstra Kurikulum Perkhidmatan', options=EXTRA_KOKO_PERKHIDMATAN.keys())
+    user_data['Ekstra Kurikulum Anugerah Khas'] = st.selectbox('Ekstra Kurikulum Anugerah Khas', options=EXTRA_KOKO_ANUGERAH.keys())
+
+def get_total(i):
+
+    return (user_data['Kehadiran Kelab'][i] / 20 * 50 + 
+        JAWATAN_KELAB[user_data['Jawatan Kelab'][i]] +
+        PERINGKAT_KELAB[user_data['Peringkat Kelab'][i]] +
+        PENCAPAIAN_KELAB[user_data['Peringkat Pencapaian Kelab'][i]] + 
+        JAWATAN_PROJEK[user_data['Jawatan Projek'][i]] + 
+        PERINGKAT_PROJEK[user_data['Peringkat Projek'][i]])
 
 st.markdown('---')
 
+# Table Display
 st.html(f'''<table style="width: 100%;">
     <tr>
-        <th style="border: 1px solid white">ELEMEN</th>
-        <th style="border: 1px solid white">ASPEK</th>
-        <th style="border: 1px solid white" colspan=2>SUKAN/PERMAINAN</th>
-        <th style="border: 1px solid white" colspan=2>KELAB/PERSATUAN</th>
-        <th style="border: 1px solid white" colspan=2>BADAN BERUNIFORM</th>
-        <th style="border: 1px solid white">EKSTRA KURIKULUM</th>
+        <th style="border: 1px solid white; padding: 10px;">ELEMEN</th>
+        <th style="border: 1px solid white; padding: 10px;">ASPEK</th>
+        <th style="border: 1px solid white; padding: 10px;" colspan=2>SUKAN/PERMAINAN</th>
+        <th style="border: 1px solid white; padding: 10px;" colspan=2>KELAB/PERSATUAN</th>
+        <th style="border: 1px solid white; padding: 10px;" colspan=2>BADAN BERUNIFORM</th>
+        <th style="border: 1px solid white; padding: 10px;">EKSTRA KURIKULUM</th>
     </tr>
     <tr>
-        <td style="border: 1px solid white" rowspan=4>PENGLIBATAN</td>
-        <td style="border: 1px solid white">Kehadiran</td>
-        <td style="border: 1px solid white">{user_data['Kehadiran Kelab'][0]} / 20</td>
-        <td style="border: 1px solid white">{user_data['Kehadiran Kelab'][0] / 20 * 50}</td>
-        <td style="border: 1px solid white">{user_data['Kehadiran Kelab'][1]} / 20</td>
-        <td style="border: 1px solid white">{user_data['Kehadiran Kelab'][1] / 20 * 50}</td>
-        <td style="border: 1px solid white">{user_data['Kehadiran Kelab'][2]} / 20</td>
-        <td style="border: 1px solid white">{user_data['Kehadiran Kelab'][2] / 20 * 50}</td>
-        <td style="border: 1px solid white">Jawatan</td>
+        <td style="border: 1px solid white; padding: 10px;" rowspan=4>PENGLIBATAN</td>
+        <td style="border: 1px solid white; padding: 10px;">Kehadiran</td>
+        <td style="border: 1px solid white; padding: 10px;">{user_data['Kehadiran Kelab'][0]} / 20</td>
+        <td style="border: 1px solid white; padding: 10px;">{user_data['Kehadiran Kelab'][0] / 20 * 50}</td>
+        <td style="border: 1px solid white; padding: 10px;">{user_data['Kehadiran Kelab'][1]} / 20</td>
+        <td style="border: 1px solid white; padding: 10px;">{user_data['Kehadiran Kelab'][1] / 20 * 50}</td>
+        <td style="border: 1px solid white; padding: 10px;">{user_data['Kehadiran Kelab'][2]} / 20</td>
+        <td style="border: 1px solid white; padding: 10px;">{user_data['Kehadiran Kelab'][2] / 20 * 50}</td>
+        <td style="border: 1px solid white; padding: 10px;" rowspan=4>{user_data['Ekstra Kurikulum Perkhidmatan']}</td>
     </tr>
     <tr>
-        <td style="border: 1px solid white">Jawatan</td>
-        <td style="border: 1px solid white">{user_data['Jawatan Kelab'][0]}</td>
-        <td style="border: 1px solid white">{JAWATAN_KELAB[user_data['Jawatan Kelab'][0]]}</td>
-        <td style="border: 1px solid white">{user_data['Jawatan Kelab'][1]}</td>
-        <td style="border: 1px solid white">{JAWATAN_KELAB[user_data['Jawatan Kelab'][1]]}</td>
-        <td style="border: 1px solid white">{user_data['Jawatan Kelab'][2]}</td>
-        <td style="border: 1px solid white">{JAWATAN_KELAB[user_data['Jawatan Kelab'][2]]}</td>
-        <td style="border: 1px solid white" rowspan=3></td>
+        <td style="border: 1px solid white; padding: 10px;">Jawatan</td>
+        <td style="border: 1px solid white; padding: 10px;">{user_data['Jawatan Kelab'][0]}</td>
+        <td style="border: 1px solid white; padding: 10px;">{JAWATAN_KELAB[user_data['Jawatan Kelab'][0]]}</td>
+        <td style="border: 1px solid white; padding: 10px;">{user_data['Jawatan Kelab'][1]}</td>
+        <td style="border: 1px solid white; padding: 10px;">{JAWATAN_KELAB[user_data['Jawatan Kelab'][1]]}</td>
+        <td style="border: 1px solid white; padding: 10px;">{user_data['Jawatan Kelab'][2]}</td>
+        <td style="border: 1px solid white; padding: 10px;">{JAWATAN_KELAB[user_data['Jawatan Kelab'][2]]}</td>
     </tr>
     <tr>
-        <td style="border: 1px solid white">Peringkat</td>
-        <td style="border: 1px solid white">{user_data['Peringkat Kelab'][0]}</td>
-        <td style="border: 1px solid white">{PERINGKAT_KELAB[user_data['Peringkat Kelab'][0]]}</td>
-        <td style="border: 1px solid white">{user_data['Peringkat Kelab'][1]}</td>
-        <td style="border: 1px solid white">{PERINGKAT_KELAB[user_data['Peringkat Kelab'][1]]}</td>
-        <td style="border: 1px solid white">{user_data['Peringkat Kelab'][2]}</td>
-        <td style="border: 1px solid white">{PERINGKAT_KELAB[user_data['Peringkat Kelab'][2]]}</td>
+        <td style="border: 1px solid white; padding: 10px;">Peringkat</td>
+        <td style="border: 1px solid white; padding: 10px;">{user_data['Peringkat Kelab'][0]}</td>
+        <td style="border: 1px solid white; padding: 10px;">{PERINGKAT_KELAB[user_data['Peringkat Kelab'][0]]}</td>
+        <td style="border: 1px solid white; padding: 10px;">{user_data['Peringkat Kelab'][1]}</td>
+        <td style="border: 1px solid white; padding: 10px;">{PERINGKAT_KELAB[user_data['Peringkat Kelab'][1]]}</td>
+        <td style="border: 1px solid white; padding: 10px;">{user_data['Peringkat Kelab'][2]}</td>
+        <td style="border: 1px solid white; padding: 10px;">{PERINGKAT_KELAB[user_data['Peringkat Kelab'][2]]}</td>
     </tr>
     <tr>
-        <td style="border: 1px solid white">Pencapaian</td>
-        <td style="border: 1px solid white">{user_data['Peringkat Pencapaian Kelab'][0]}</td>
-        <td style="border: 1px solid white">{PERINGKAT_KELAB[user_data['Peringkat Pencapaian Kelab'][0]] + PENCAPAIAN[user_data["Pencapaian Kelab"][0]]}</td>
-        <td style="border: 1px solid white">{user_data['Peringkat Pencapaian Kelab'][1]}</td>
-        <td style="border: 1px solid white">{PERINGKAT_KELAB[user_data['Peringkat Pencapaian Kelab'][1]] + PENCAPAIAN[user_data["Pencapaian Kelab"][1]]}</td>
-        <td style="border: 1px solid white">{user_data['Peringkat Pencapaian Kelab'][2]}</td>
-        <td style="border: 1px solid white">{PERINGKAT_KELAB[user_data['Peringkat Pencapaian Kelab'][2]] + PENCAPAIAN[user_data["Pencapaian Kelab"][2]]}</td>
+        <td style="border: 1px solid white; padding: 10px;">Pencapaian</td>
+        <td style="border: 1px solid white; padding: 10px;">{user_data['Peringkat Pencapaian Kelab'][0]}</td>
+        <td style="border: 1px solid white; padding: 10px;">{PENCAPAIAN_KELAB[user_data['Peringkat Pencapaian Kelab'][0]]}</td>
+        <td style="border: 1px solid white; padding: 10px;">{user_data['Peringkat Pencapaian Kelab'][1]}</td>
+        <td style="border: 1px solid white; padding: 10px;">{PENCAPAIAN_KELAB[user_data['Peringkat Pencapaian Kelab'][1]]}</td>
+        <td style="border: 1px solid white; padding: 10px;">{user_data['Peringkat Pencapaian Kelab'][2]}</td>
+        <td style="border: 1px solid white; padding: 10px;">{PENCAPAIAN_KELAB[user_data['Peringkat Pencapaian Kelab'][2]]}</td>
+    </tr>
+    <tr>
+        <td style="border: 1px solid white; padding: 10px;" rowspan=3>PENGURUSAN PROJEK</td>
+        <td style="border: 1px solid white; padding: 10px;">Nama Projek</td>
+        <td style="border: 1px solid white; padding: 10px;" colspan=2>{user_data['Nama Projek'][0]}</td>
+        <td style="border: 1px solid white; padding: 10px;" colspan=2>{user_data['Nama Projek'][1]}</td>
+        <td style="border: 1px solid white; padding: 10px;" colspan=2>{user_data['Nama Projek'][2]}</td>
+        <td style="border: 1px solid white; padding: 10px;" rowspan=3>{user_data['Ekstra Kurikulum Anugerah Khas']}</td>
+    </tr>    
+    <tr>
+        <td style="border: 1px solid white; padding: 10px;">Jawatan</td>
+        <td style="border: 1px solid white; padding: 10px;">{user_data['Jawatan Projek'][0]}</td>
+        <td style="border: 1px solid white; padding: 10px;">{JAWATAN_PROJEK[user_data['Jawatan Projek'][0]]}</td>
+        <td style="border: 1px solid white; padding: 10px;">{user_data['Jawatan Projek'][1]}</td>
+        <td style="border: 1px solid white; padding: 10px;">{JAWATAN_PROJEK[user_data['Jawatan Projek'][1]]}</td>
+        <td style="border: 1px solid white; padding: 10px;">{user_data['Jawatan Projek'][2]}</td>
+        <td style="border: 1px solid white; padding: 10px;">{JAWATAN_PROJEK[user_data['Jawatan Projek'][2]]}</td>
+    </tr>
+    <tr>
+        <td style="border: 1px solid white; padding: 10px;">Peringkat</td>
+        <td style="border: 1px solid white; padding: 10px;">{user_data['Peringkat Projek'][0]}</td>
+        <td style="border: 1px solid white; padding: 10px;">{PERINGKAT_PROJEK[user_data['Peringkat Projek'][0]]}</td>
+        <td style="border: 1px solid white; padding: 10px;">{user_data['Peringkat Projek'][1]}</td>
+        <td style="border: 1px solid white; padding: 10px;">{PERINGKAT_PROJEK[user_data['Peringkat Projek'][1]]}</td>
+        <td style="border: 1px solid white; padding: 10px;">{user_data['Peringkat Projek'][2]}</td>
+        <td style="border: 1px solid white; padding: 10px;">{PERINGKAT_PROJEK[user_data['Peringkat Projek'][2]]}</td>
+    </tr>
+    <tr>
+        <td style="border: 1px solid white; padding: 10px;" colspan=2>SKOR</td>
+        <td style="border: 1px solid white; padding: 10px;" colspan=2>{get_total(0)}</td>
+        <td style="border: 1px solid white; padding: 10px;" colspan=2>{get_total(1)}</td>
+        <td style="border: 1px solid white; padding: 10px;" colspan=2>{get_total(2)}</td>
+        <td style="border: 1px solid white; padding: 10px;">{max(
+            EXTRA_KOKO_PERKHIDMATAN[user_data['Ekstra Kurikulum Perkhidmatan']],
+            EXTRA_KOKO_ANUGERAH[user_data['Ekstra Kurikulum Anugerah Khas']],
+        )}</td>
+    </tr>
+    <tr>
+        <td style="border: 1px solid white; padding: 10px;" colspan=2>JUMLAH SKOR (%)</td>
+        <td style="border: 1px solid white; padding: 10px;" colspan=7>{sum(
+            sorted([get_total(0), get_total(1), get_total(2)], reverse=True)[:2]
+        ) / 2 + max(
+            EXTRA_KOKO_PERKHIDMATAN[user_data['Ekstra Kurikulum Perkhidmatan']],
+            EXTRA_KOKO_ANUGERAH[user_data['Ekstra Kurikulum Anugerah Khas']],
+        )}</td>
     </tr>
 </table>
 ''')
---
+++ login.py
@@ -0,0 +1,28 @@
+import hmac
+import streamlit as st
+
+def user_login():
+
+    def login_form():
+        with st.form("Credentials"):
+            st.text_input("Username", key="username")
+            st.text_input("Password", type="password", key="password")
+            st.form_submit_button("Log in", on_click=password_entered)
+
+    def password_entered():
+        if st.session_state["username"] == st.secrets["username"] and hmac.compare_digest(st.session_state["password"], st.secrets["password"]):
+            st.session_state["password_correct"] = True
+            del st.session_state["password"]
+            del st.session_state["username"]
+        else:
+            st.session_state["password_correct"] = False
+
+    # The username + password is validated.
+    if st.session_state.get("password_correct", False):
+        return True
+
+    # Show inputs for username + password.
+    login_form()
+    if "password_correct" in st.session_state:
+        st.error("😕 User not known or password incorrect")
+    return False
