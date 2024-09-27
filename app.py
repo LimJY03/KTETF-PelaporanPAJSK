c505949865225fab95ce8b9c6b685458c7453d25
Lim JY <limjunyi03@gmail.com>
2024-09-27 01:44:19 +0000

    Added required selectbox data

--- app.py
+++ app.py
@@ -80,13 +80,30 @@ EXTRA_KOKO_PERKHIDMATAN = {
     'Tiada': 0,
     'Ketua Murid / Pengerusi': 10,
     'Penolong Ketua Murid / Timbalan Pengerusi': 8,
+    'Pengurusan Utama yang ditubuhkan untuk membantu sekolah/Exco Persatuan Tingkatan Enam/ Pengawas/ Ketua Asrama / BADAR/Ketua PRS/ Pengerusi Badan-Badan Perkhidmatan Sekolah ': 7,
+    'Penolong Ketua Asrama/ BADAR/Pengawas Pusat Sumber/ Ahli Lembaga Pengarah Koperasi Sekolah': 6,
+    'AJK Utama  Badan-badan Perkhidmatan atau Pengurusan/ Bendahari/ Setiausaha/ Ketua Biro - Asrama/BADAR/ PRS / Pegawas Koperasi/ Ketua Kelas / Ketua Tingkatan': 4,
+    'Ahli-ahli J/Kuasa Perkhidmatan dan Sokongan - AJK Kecil Biro / Ketua Bilik / Ketua Dorm /BADAR/Penolong Ketua Kelas / Penolong Ketua Bilik': 3,
 }
 
 EXTRA_KOKO_ANUGERAH = {
     'Tiada': 0,
-    'ARP Emas': 10,
-    'ARP Perak': 7,
-    'ARP Gangsa': 5,
+    'Anugerah Remaja Perdana (ARP) Anjuran Kementerian Belia dan Sukan - Emas': 10,
+    'Anugerah Remaja Perdana (ARP) Anjuran Kementerian Belia dan Sukan - Perak': 7,
+    'Anugerah Remaja Perdana (ARP) Anjuran Kementerian Belia dan Sukan - Gangsa': 5,
+    'Anugerah Khas Sukan - Peringkat Kebangsaan': 5,
+    'Anugerah Khas Sukan - Peringkat Negeri': 4,
+    'Anugerah Khas Sukan - Peringkat Zon/Daerah/Bahagian': 3,
+    'Anugerah Khas Sukan - Peringkat Sekolah': 2,
+    'Penyertaan Peringkat Antarabangsa - Emas': 5,
+    'Penyertaan Peringkat Antarabangsa - Perak': 4,
+    'Penyertaan Peringkat Antarabangsa - Gangsa': 3,
+    'Penyertaan Peringkat Antarabangsa - Penyertaan': 2,
+    'Anugerah-anugerah lain/ Penghargaan Khas Setara - Peringkat Kebangsaan': 5,
+    'Anugerah-anugerah lain/ Penghargaan Khas Setara - Peringkat Negeri': 4,
+    'Anugerah-anugerah lain/ Penghargaan Khas Setara - Peringkat Zon/Daerah/Bahagian': 3,
+    'Anugerah-anugerah lain/ Penghargaan Khas Setara - Penyertaan': 2,
+    'Pengakap Raja/ Pandu Puteri Raja': 10,
 }
 
 # Logic
